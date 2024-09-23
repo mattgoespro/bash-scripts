ef4068ee7fd93efab7b22784049bde0062075032
hoppingmode <mattyoung305@gmail.com>
2024-09-23 09:33:28 +0200

    cp

--- Desktop Customization/Cursors/InstallCursors.ps1
+++ Desktop Customization/Cursors/InstallCursors.ps1
@@ -1,131 +1,72 @@
-<#
- # Script to install fonts from an archive containing ZIPs of fonts for different font families.
- #>
-
-# Admin rights are required to install fonts to the system
+# Ensure the script is run with administrative privileges
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Host 'This script requires administrative privileges. Please run as administrator.'
     Pause
-    exit
+    exit 1
 }
 
-# Function to install fonts from a given directory
-function Install-FontsFromDir {
-    param (
-        [string]$directory,
-        [bool]$shouldInstall
-    )
-    Get-ChildItem -Path $directory -Filter *.ttf -Recurse | ForEach-Object {
-        $destinationPath = "$env:SystemRoot\Fonts\$($_.Name)"
+# Extract the archives found in `Archives` directory relative to this script, then
+# execute the `.inf` installation file to install the cursor pack.
 
-        if ($shouldInstall) {
-            if ( -not (Test-Path -Path $destinationPath)) {
-                Copy-Item -Path $_.FullName -Destination $destinationPath
+$scriptDir = $PSScriptRoot
+$cursor_pack_dir = Join-Path -Path $scriptDir -ChildPath 'Archives'
 
-                $fontRegPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts'
-                $fontName = $_.BaseName
-
-                try {
-                    New-ItemProperty -Path $fontRegPath -Name "$fontName (TrueType)" -PropertyType String -Value $_.Name -Force
-                    Write-Host "Installed font: $($_.Name)"
-                    continue
-                } catch {
-                    Write-Host "Failed to register font: $fontName"
-                    continue
-                }
-            }
-
-            Write-Host "Font already exists: $($_.Name)"
-        }
-
-        if (!($shouldInstall)) {
-            try {
-                if (Test-Path -Path $destinationPath) {
-                    Remove-Item -Path $destinationPath -Force
-                    Write-Output "Removed font file: $($_.Name)"
-                }
-
-                # remove registry entry if it exists
-                if (Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts\$($_.BaseName) (TrueType)") {
-                    Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts' -Name "$($_.BaseName) (TrueType)" -Force
-                    Write-Output "Removed font registry entry: $($_.BaseName)"
-                }
-
-                Write-Output "Uninstalled font: $($_.Name)"
-            } catch {
-                Write-Host "Failed to uninstall font: $($_.Name)"
-            }
-        }
-    }
+if (-not (Test-Path $cursor_pack_dir)) {
+    Write-Host "Installation directory does not exist: $cursor_pack_dir"
+    Pause
+    exit 1
 }
 
-function Main {
-    try {
-        # Temporary extraction directory
-        $tempDir = "$env:TEMP\Fonts"
-        Write-Host "Creating temporary directory: $tempDir"
-        New-Item -ItemType Directory -Path $tempDir -Force
+try {
+    $archives = Get-ChildItem -Path $cursor_pack_dir -Filter '*.zip' -ErrorAction Stop
 
-        $fontsZipPath = $args[0]
-
-        if (-not $fontsZipPath) {
-            $fontsZipPath = Read-Host 'Enter the path to the font ZIP archive...'
-        }
-
-        if (-not (Test-Path -Path $fontsZipPath)) {
-            Write-Host "The specified zip file does not exist at path: $fontsZipPath."
-            Pause
-            exit 1
-        }
-
-        # Extract Fonts.zip to the temporary directory
-        Write-Host "Extracting fonts from: $fontsZipPath to $tempDir"
-        Expand-Archive -Path $fontsZipPath -DestinationPath $tempDir -Force
-
-        $installPrompt = Read-Host 'Font installer. Uninstall/Install fonts? (U/I)'
-        Write-Output "Prompt: $installPrompt"
-        $shouldInstallFonts = $false
-
-        if ( $installPrompt -eq '' -or $installPrompt -eq 'I' -or $installPrompt -eq 'i') {
-            $shouldInstallFonts = $true
-            Write-Output 'Installing fonts...'
-        }
-
-        # Iterate through each ZF (zip files) in the temporary directory
-        Get-ChildItem -Path $tempDir -Filter *.zip | ForEach-Object {
-            $zfPath = $_.FullName
-            $zfExtractDir = "$tempDir\$($_.BaseName)"
-            Write-Host "Processing zip file: $zfPath"
-
-            if ($shouldInstallFonts) {
-                # Extract ZF
-                Write-Host "Extracting zip file: $zfPath to $zfExtractDir"
-                Expand-Archive -Path $zfPath -DestinationPath $zfExtractDir -Force
-            }
-
-            # Install fonts from the root of ZF
-            Install-FontsFromDir -directory $zfExtractDir  -shouldInstall:$shouldInstallFonts
-
-            # Check and install fonts from the static directory if it exists
-            $staticDir = "$zfExtractDir\static"
-            if (Test-Path -Path $staticDir) {
-                Install-FontsFromDir -directory $staticDir   -shouldInstall:$shouldInstallFonts
-            }
-
-            # Clean up extracted ZF directory
-            Write-Host "Cleaning up extracted directory: $zfExtractDir"
-            Remove-Item -Path $zfExtractDir -Recurse -Force
-        }
-
-        # Clean up the temporary directory
-        Write-Host "Cleaning up temporary directory: $tempDir"
-        Remove-Item -Path $tempDir -Recurse -Force
-    } catch {
-        Write-Host "An error occurred: $_"
-    } finally {
+    if ($archives.Count -eq 0) {
+        Write-Host 'No cursor packs to install. Place the cursor packs in the `Archives` directory to install.'
         Pause
+        exit 1
     }
-}
 
-Main
-exit 0
+    Write-Host "Found $($archives.Count) cursor packs to install."
+
+    $tempDir = New-Item -ItemType Directory -Path "$env:TEMP\InstallCursors" -Force
+
+    Write-Host "Created temporary directory $tempDir"
+
+    Pause
+    foreach ($archive in $archives) {
+        Write-Host "Installing $($archive.Name)..."
+        # Add the rest of your installation logic here
+
+        # Extract the archive
+        Expand-Archive -Path $archive.FullName -DestinationPath $tempDir.FullName -Force
+
+        # Get the extracted directory
+        $extractedDir = Get-ChildItem -Path $tempDir.FullName -Directory | Select-Object -First 1
+
+        # Get the `.inf` file
+        $infFile = Join-Path -Path $extractedDir.FullName -ChildPath '*.inf' | Get-Item
+
+        if (-not $infFile) {
+            Write-Host "Installation file not found: $infFile"
+            Write-Host "Create an installation file with the extension `.inf` then rerun this script."
+            continue
+        }
+
+        # Install the cursor pack
+        Write-Host "Installing $($infFile.Name)..."
+        Start-Process -FilePath 'cmd.exe' -ArgumentList "/c ${infFile.FullName}" -Wait -NoNewWindow
+
+        Write-Host "Successfully installed $($archive.Name)"
+    }
+
+    Write-Host 'Cleaning up...'
+    Remove-Item -Path $tempDir.FullName -Recurse -Force
+
+    Write-Host 'Installation complete.'
+    Read-Host -Prompt 'Press [Enter] to exit...'
+    exit 0
+} catch {
+    Write-Host "An error occurred: $_"
+    Pause
+    exit 1
+}
---
+++ Programs/Scripts/InstallTools.ps1
@@ -0,0 +1,64 @@
+function installChocolatey () {
+    $InstallDir = 'C:\ProgramData\chocoportable'
+    $env:ChocolateyInstall = "$InstallDir"
+
+    # Allow execution of scripts
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+function installWithWinget ($package) {
+    # Check if `winget` is installed
+    if (-not (Get-Command winget -ErrorAction Stop)) {
+        Write-Host 'Please install App Installer from the Microsoft Store to continue.'
+        Pause
+    }
+
+    winget install $package
+}
+
+function installWithVolta ($package) {
+    # check if volta is installed
+    if (-not (Get-Command volta -ErrorAction Stop)) {
+        Write-Host 'Installing required Volta package manager...'
+        winget install Volta.Volta
+    }
+
+    volta install $package
+}
+
+function installWithChocolatey ($package) {
+    # check if choco is installed
+    if (-not (Get-Command choco -ErrorAction Stop)) {
+        Write-Host 'Installing Chocolatey package manager...'
+        installChocolatey
+    }
+
+    choco install $package -y
+}
+
+function installWithPip ($package) {
+    # check if pip is installed
+    if (-not (Get-Command pip -ErrorAction Stop)) {
+        # Install latest version of Python
+        choco install python -y
+    }
+
+    pip install --upgrade pip
+    pip install $package
+}
+
+
+# Chocolatey
+installWithChocolatey 'python'
+
+# Winget
+installWithWinget 'ffmpeg'
+installWithWinget 'shfmt'
+
+# Volta
+installWithVolta 'node'
+installWithVolta 'shellcheck'
+installWithPip 'pylint'
+installWithPip 'black'
